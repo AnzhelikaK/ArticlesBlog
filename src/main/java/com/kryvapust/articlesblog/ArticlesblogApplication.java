d5559400f3b67c26407befdd78bc362996510a86
AnzhelikaK <AnzhelikaShch11@gmail.com>
2020-05-29 15:29:20 +0300

    initial

---
+++ src/main/java/com/kryvapust/articlesblog/ArticlesblogApplication.java
@@ -0,0 +1,13 @@
+package com.kryvapust.articlesblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kryvapust/articlesblog/ArticlesblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryvapust.articlesblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
